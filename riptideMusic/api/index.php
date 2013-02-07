29e3ff0e4b8ec08ab2a8ab5be170fd260b34dac9
Tomas Celaya <tjcelaya@gmail.com>
2013-02-06 22:33:05 -0500

    adding initial album route, SQL tested and worked, but is being rethought

--- riptideMusic/api/index.php
+++ riptideMusic/api/index.php
@@ -28,6 +28,43 @@ $app->get('/', function() use ($sqlConnection, $dbPassword) {
   echo gettype($dbPassword);
 })->name('index');
 
+$app->get('/album/:artist', function($artist) use ($sqlConnection) {
+  
+  $artistDetails = array();
+
+  $artistQuery = mysqli_query(
+    $sqlConnection,
+    "select * from Albums where artistID in (select artistID from Artists where artistName='Michael Jackson' AND albumName='Thriller');"
+    );
+
+  //get them record by record into the result object
+  if($artistQuery->num_rows > 0) {
+      while($row = $artistQuery->fetch_assoc()) {
+          array_push($artistDetails, $row);
+      }
+  }
+  else {
+      echo json_encode(array('err' =>'NO RESULTS'));
+  }
+
+  $tagQuery = mysqli_query(
+    $sqlConnection,
+    "select * from AlbumTags where albumID in (select artistID from Artists where artistName='$artist')"
+    );
+
+  if($tagQuery->num_rows > 0) {
+      while($row = $tagQuery->fetch_assoc()) {
+          array_push($artistDetails, $row);
+      }
+  }
+  else {
+    echo json_encode(array('err' => 'NO TAGS'));
+  }
+
+  echo json_encode($artistDetails);
+
+});
+
 require 'users.php';
 require 'music-engine.php';
 
