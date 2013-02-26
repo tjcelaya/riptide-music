d4db0c09475b9b5317a0335da3806d5b9971ecca
Angela Gibbens <gibbens@ww2.cs.fsu.edu>
2013-02-26 12:51:15 -0500

    Testing to see if I can commit a change using git 	modified:   index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-<?php require "header.php"; ?>
+<?php require "header.php"; ?> 
       <div class="row-fluid inner-row-div">
         <div class='span2'></div>
         <div class='span7 main-body'>
           <a href="/~celaya/riptideMusic/artist.php?name=Michael Jackson">
             <p>albums by Mike</p>
           </a>
-	// test
+	// test test 2
         </div>
         <div class='span2 rec'>
           <p>try out the search bar!</p>
