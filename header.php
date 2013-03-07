77d23f443ffed32011438f980f7c9c3e4757ddb8
Tomas Celaya <tjcelaya@gmail.com>
2013-03-07 00:37:03 -0500

    added sample bootstrap user button

--- header.php
+++ header.php
@@ -126,6 +126,17 @@ $smarty->debugging = true;
 
 
           <a class="brand" href="/~celaya/riptideMusic/">RIPTIDE MUSIC</a>
+          <div class="btn-group">
+            <a class="btn btn-primary" href="#"><i class="icon-user icon-white"></i> User</a>
+            <a class="btn btn-primary dropdown-toggle" data-toggle="dropdown" href="#"><span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="#"><i class="icon-pencil"></i> Edit</a></li>
+              <li><a href="#"><i class="icon-trash"></i> Delete</a></li>
+              <li><a href="#"><i class="icon-ban-circle"></i> Ban</a></li>
+              <li class="divider"></li>
+              <li><a href="#"><i class="i"></i> Make admin</a></li>
+            </ul>
+          </div>
           <span class="brand"><?php echo json_encode(isUserLoggedIn()); ?></span>
           <div class='pull-right'>
             <form class="form-search" action="/~celaya/riptideMusic/search.php" method='GET'>
