06900b17f21517f0378cc1a3166e81945132f4cd
Tomas Celaya <tjcelaya@gmail.com>
2013-03-23 17:14:55 -0400

    fixed login dropdown

--- header.php
+++ header.php
@@ -15,7 +15,7 @@ $smarty->compile_dir = "templates/compiled";
 $smarty->cache_dir = "templates/cached";
 $smarty->config_dir = "smarty-config";
 $smarty->error_unassigned = false;
-$smarty->debugging = true;
+// $smarty->debugging = true;
 
 ?>
 <!DOCTYPE html>
@@ -126,18 +126,7 @@ $smarty->debugging = true;
 
 
           <a class="brand" href="/~celaya/riptideMusic/">RIPTIDE MUSIC</a>
-          <div class="btn-group">
-            <a class="btn btn-primary" href="#"><i class="icon-user icon-white"></i> User</a>
-            <a class="btn btn-primary dropdown-toggle" data-toggle="dropdown" href="#"><span class="caret"></span></a>
-            <ul class="dropdown-menu">
-              <li><a href="#"><i class="icon-pencil"></i> Edit</a></li>
-              <li><a href="#"><i class="icon-trash"></i> Delete</a></li>
-              <li><a href="#"><i class="icon-ban-circle"></i> Ban</a></li>
-              <li class="divider"></li>
-              <li><a href="#"><i class="i"></i> Make admin</a></li>
-            </ul>
-          </div>
-          <span class="brand"><?php echo json_encode(isUserLoggedIn()); ?></span>
+          <span class="brand"><?php echo (int)(isUserLoggedIn()); ?></span>
           <div class='pull-right'>
             <form class="form-search" action="/~celaya/riptideMusic/search.php" method='GET'>
               <div class="input-append">
