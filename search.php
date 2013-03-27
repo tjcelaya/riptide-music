037c5afac60b6ca44b110d2dc14f6bfd565d88bd
Tomas Celaya <tjcelaya@gmail.com>
2013-03-26 22:34:54 -0400

    extracted discog search and added a flush() to show internal results as quickly as possible

--- search.php
+++ search.php
@@ -28,34 +28,11 @@
       {
         echo "no request";
       }
+      flush();
     ?>
 
     <?php
-      if (isset($_GET["q"]))
-      {
-
-        echo $apiUrl =   
-          "http://ww2.cs.fsu.edu/~celaya/".
-          "riptideMusic/api/dsearch/".urlencode($_GET['q']);
-
-        $APIresponse = 
-            json_decode(file_get_contents($apiUrl), true);
-        
-        });
-        
-
-        foreach ($APIresponse as $album) {
-          foreach ($album as $kk => $vv) {
-            $smarty->assign($kk, $vv);
-          }
-          $smarty->display('album-create.tpl');
-        }
-
-      } 
-      else
-      {
-        echo "no request";
-      }
+    require 'discogSearch.php';
     ?>
   </div>
 </div>
