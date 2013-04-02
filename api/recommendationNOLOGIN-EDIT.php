dc35ee42a225828c720cd4cf5b5f3d896f636564
Angela Gibbens <gibbens@ww2.cs.fsu.edu>
2013-04-01 22:10:49 -0400

    Committer: Angela Gibbens <gibbens@ww2.cs.fsu.edu> 	modified:   recommendationNOLOGIN-EDIT.php

---
+++ api/recommendationNOLOGIN-EDIT.php
@@ -0,0 +1,40 @@
+<?php
+  //Recommendation for a non logged in user based off an albums page                                                                
+$app->get('/recommendation/album/:id', function($id) use ($sqlConnection) {//uses slim framework                                  
+
+    $sqlQueryResult = array();
+
+  $album =
+    get_sql_results(
+		    $sqlQueryResult,
+		    $sqlConnection,
+      "select tagName, weight from AlbumTags where albumID=$id group by weight limit 0, 6"
+		    ); // gets the album from the database                                                                                        
+
+  $results= array(); // creates a place for results                                                                                
+
+  if($album)// If it exists find album with same tag                                                                             
+    {foreach $sqlQueryResult['tag'] as $kk => $tag) {
+    $query= get_sql_results(
+			    $results,
+			    $sqlConnection,
+        "select albumID from AlbumTags where tagName =$tag limit 0, 3"
+
+			    );
+
+    if(!$query){ //if no result                                                                                                       
+      echo json_encode(array('err'=>'no RESULTS'));
+      return;}
+
+    echo json_encode($results)
+      // encode results and return                                                                                                 
+      return;
+  }
+        else
+	  {
+	    // if album does not exist                                                                                                        
+	    echo json_encode(array('err'=>'no ALBUMS'));
+	    return;
+	  }
+
+  });
