098a107aebe39f8cb366f284b660d514b23078ba
Angela Gibbens <gibbens@ww2.cs.fsu.edu>
2013-04-03 17:14:46 -0400

    Got it working finally! 	modified:   recommendationNOLOGIN-EDIT.php

--- api/recommendationNOLOGIN-EDIT.php
+++ api/recommendationNOLOGIN-EDIT.php
@@ -1,9 +1,10 @@
 <?php
   //Recommendation for a non logged in user based off an albums page                                                                
-$app->get('/recommendation/album/:id', function($id) use ($sqlConnection) {//uses slim framework                                  
+$app->get('/recommendation/album/:id', function($id) use ($sqlConnection) {
+//uses slim framework                                  
 
     $sqlQueryResult = array();
-
+    $results=array();
   $album =
     get_sql_results(
 		    $sqlQueryResult,
@@ -11,23 +12,39 @@ $app->get('/recommendation/album/:id', function($id) use ($sqlConnection) {//use
       "select tagName, weight"." from AlbumTags"." where albumID={$id}".
 		    " order by weight DESC limit 0, 6"
 		    ); // gets the album from the database                                                                                        
-
-  $results= array(); // creates a place for results                                                                                
-
-  if($album)// If it exists find album with same tag                                                                             
-    {foreach $sqlQueryResult as $kk => $tag) {
+  if($album)// If it exists find album with same tag                                                                                                                                  
+    {foreach ($sqlQueryResult as $kk => $tag) {
+	//var_dump($tag);
+    // echo "<br>";
+      $myq = "select albumID from AlbumTags where tagName ='{$tag['tagName']}' limit 0, 3";
+      //  $myq = "select albumID"." from AlbumTags"." where tagName ='Jackson'";
+      // echo "trying $myq<br>";
     $query= get_sql_results(
 			    $results,
 			    $sqlConnection,
-        "select albumID from AlbumTags where tagName ={$tag} limit 0, 3"
-
+                            $myq
 			    );
+    //    var_dump($results);
+    // if ($query)
+      //echo "is: <br>";
+    // else
+    // echo "is not true<br>";    
+  }
+      //    var_dump($results);                                                             
+      $dups = array();
+      foreach ($results as $t => $tn)
+	{
+	  if (in_array($tn, $dups))
+	    unset($results[$t]);
+	  else
+	    array_push($dups,$tn);
+	}
 
     if(!$query){ //if no result                                                                                                       
       echo json_encode(array('err'=>'no RESULTS'));
       return;}
 
-    echo json_encode($results)
+    echo json_encode($results);
       // encode results and return                                                                                                 
       return;
   }
