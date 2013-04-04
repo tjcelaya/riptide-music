a5fa2405fef15fb0eec2f02918b7e18f3c743510
Tomas Celaya <tjcelaya@gmail.com>
2013-04-03 20:28:41 -0400

    JASON: Genre page fully functional

--- api/genre-engine.php
+++ api/genre-engine.php
@@ -1,28 +1,40 @@
 <?php
 
-
-// view genre
+// view genre name & description
 $app->get('/genre/:genreName', function($genreName) use ($sqlConnection)
   { 
     if(!isset($genreName))
-    { echo json_encode(array('err'=>'NO params'));
+    { echo json_encode(array('err'=>'No parameters'));
       return;
     }
 
     $queryDetails = array();
-    $sqlGenreSuccess = getGenre($queryDetails, $genreName, $sqlConnection);
+    $sqlGenreSuccess = getGenreInformation($queryDetails, $genreName, $sqlConnection);
 
     if($sqlGenreSuccess)
-      $sqlAlbumSuccess = getAlbums($queryDetails, $genreName, $sqlConnection);
+      echo json_encode($queryDetails);
     else
-      echo json_encode(array('err'=>'SQLerr'));
+      echo json_encode(array('err'=>'Genre was not retrieved'));
+      
+});
 
+
+// view album examples for genre
+$app->get('/genre/albums/:genreName', function($genreName) use ($sqlConnection)
+{
+    if(!isset($genreName))
+    {
+      echo json_encode(array('err'=>'No paramters'));
+      reutrn;
+    }
+  
+    $queryDetails = array();
+    $sqlAlbumSuccess = getAlbumExamplesByGenre($queryDetails, $genreName, $sqlConnection);
+    
     if($sqlAlbumSuccess)
       echo json_encode($queryDetails);
     else
-      echo json_encode(array('err'=>'SQLerr'));
-
-
+      echo json_encode(array('err'=>'Albums were not retrieved'));
 });
 
 
@@ -48,7 +60,7 @@ $app->post('/newgenre', function() use ($sqlConnection)
 });
 
 
-function getGenre(&$result,$parameters,$sqlConnection)
+function getGenreInformation(&$result,$parameters,$sqlConnection)
 {
 	$sqlSuccess = get_sql_results($result, $sqlConnection,
 			"select * from Genres ".
@@ -56,10 +68,45 @@ function getGenre(&$result,$parameters,$sqlConnection)
 	return $sqlSuccess;
 }
 
-function getAlbums(&$result, $parameters, $sqlConnection)
+function getAlbumExamplesByGenre(&$result, $parameters, $sqlConnection)
 {
-  $sqlSuccess = get_sql_results($result, $sqlConnection,
-    "select * from GenreExamples ".
-    "where genreName = '{$parameters}'");
+  
+  //First, find the ids of each qualifying album
+  $idResult = array();
+
+  $sqlSuccess = get_sql_results($idResult, $sqlConnection,
+    		"select albumID from Albums ".
+      		"where albumID in ".
+        		"(select albumID from GenreExamples where ".
+       			"genreName = '{$parameters}')");
+  
+  if(!$sqlSuccess)
+  {
+    printf("Error: %s\n", $sqlConnection->error);
+    echo json_encode(array('err'=>'Cannot retrieve Album id'));
+    return;
+  }
+
+
+  //Then, for each of these ids, return album info
+  //Use a temporary array to store the returned album object
+  //then push it onto the main $result array.
+  $tempResult = array();  
+
+  foreach($idResult as $value)
+    foreach($value as $id)
+    {
+      $sqlSuccess = getAlbumById($tempResult, $sqlConnection, $id);
+      array_push($result, $tempResult);
+    }
+
+    
+  if(!$sqlSuccess)
+  {
+    echo json_encode(array('err'=>'Cannot retrieve Album info'));
+    return;
+  }
+
   return $sqlSuccess;
 }
+
--- genre.php
+++ genre.php
@@ -1,10 +1,45 @@
 <?php require "header.php"; ?>
 <div class='inner-row-div row-fluid'>
     <div class='row-fluid'>
-        <div class='main-body span5 offset2'>
+        <div class='main-body span8 offset1'>
 	<p>Genre</p>
 	<hr/>
 	<h1><?php echo $_GET['name'] ?></h1>
+	<?php
+	  if(isset($_GET['name']))
+	  {
+	    $apiURL =
+		"http://ww2.cs.fsu.edu/~celaya/".
+		"riptideMusic/api/genre/".urlencode($_GET['name']);
+
+	    $genreRequest = json_decode(file_get_contents($apiURL), true);
+   //USE GENRE REQUEST OBJECT WITH GENRE SMARTY TEMPLATE HERE
+
+         }  
+	?>
+
+
+	<?php
+	  if(isset($_GET['name']))
+	  {
+	    $apiURL =
+                "http://ww2.cs.fsu.edu/~celaya/".
+                "riptideMusic/api/genre/albums/".urlencode($_GET['name']);
+
+            $genreAlbumsRequest = json_decode(file_get_contents($apiURL), true);
+
+
+	    foreach($genreAlbumsRequest as $anAlbum)
+	    {
+	      foreach($anAlbum as $k => $v)
+	      {
+		$smarty->assign($k,$v);
+	      }
+		$smarty->display('album-template.tpl');
+	    }
+          }
+	?>
+
 	</div>
     </div>
 </div>
